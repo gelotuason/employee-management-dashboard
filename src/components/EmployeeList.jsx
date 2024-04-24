7207d2c2fb85809ac6b034a3be623e0c1b6f2b13
gelotuason <gelotuason@gmail.com>
2024-04-24 12:59:15 +0800

    changed side nav design

--- src/components/EmployeeList.jsx
+++ src/components/EmployeeList.jsx
@@ -50,6 +50,7 @@ export default function EmployeeList() {
                                                         />
                                                     ))
                                                 }
+
                                             </tbody>
                                         </table>
                                     </div>
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -4,7 +4,7 @@ import { AppState } from '../App';
 import firebaseApp from '../components/FirebaseConfig';
 import { getAuth, signOut } from "firebase/auth";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFacebookF, faInstagram, faLinkedinIn, faTiktok } from '@fortawesome/free-brands-svg-icons';
+import { faLinkedinIn, faGithub } from '@fortawesome/free-brands-svg-icons';
 import { faEnvelope } from '@fortawesome/free-regular-svg-icons';
 
 export default function Layout() {
@@ -36,8 +36,8 @@ export default function Layout() {
                 </div>
 
             </header>
-            <main className='flex'>
-                <aside className="min-w-16 md:w-16 lg:w-48 transition-all duration-200 rounded-ee-xl bg-gray-900 text-white border-r-2 p-3 h-full">
+            <main className='flex p-3'>
+                <aside className="min-w-16 md:w-16 lg:w-48 transition-all duration-200 border-r-2 p-3 h-full">
                     {
                         authenticated ?
                             <>
@@ -45,17 +45,17 @@ export default function Layout() {
                                     <div className='mb-20'>
                                         <small className='font-semibold'>MENU</small>
                                         {/* Employee List */}
-                                        <Link className="flex rounded gap-5 p-2 hover:bg-gray-600 font-medium" to='/'>
+                                        <Link className="flex gap-3 p-2 border-slate-600 hover:text-slate-600 font-medium focus:outline-none focus-visible:ring" to='/'>
                                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
                                                 <path fillRule="evenodd" d="M8.25 6.75a3.75 3.75 0 1 1 7.5 0 3.75 3.75 0 0 1-7.5 0ZM15.75 9.75a3 3 0 1 1 6 0 3 3 0 0 1-6 0ZM2.25 9.75a3 3 0 1 1 6 0 3 3 0 0 1-6 0ZM6.31 15.117A6.745 6.745 0 0 1 12 12a6.745 6.745 0 0 1 6.709 7.498.75.75 0 0 1-.372.568A12.696 12.696 0 0 1 12 21.75c-2.305 0-4.47-.612-6.337-1.684a.75.75 0 0 1-.372-.568 6.787 6.787 0 0 1 1.019-4.38Z" clipRule="evenodd" />
                                                 <path d="M5.082 14.254a8.287 8.287 0 0 0-1.308 5.135 9.687 9.687 0 0 1-1.764-.44l-.115-.04a.563.563 0 0 1-.373-.487l-.01-.121a3.75 3.75 0 0 1 3.57-4.047ZM20.226 19.389a8.287 8.287 0 0 0-1.308-5.135 3.75 3.75 0 0 1 3.57 4.047l-.01.121a.563.563 0 0 1-.373.486l-.115.04c-.567.2-1.156.349-1.764.441Z" />
                                             </svg>
-                                            <span className=''>Employee List</span>
+                                            <span className=''>Employees</span>
                                         </Link>
                                         {/* End of Employee List */}
 
                                         {/* Add Employee */}
-                                        <Link className="flex rounded gap-5 p-2 hover:bg-gray-600 font-medium" to='addEmployee'>
+                                        <Link className="flex gap-3 p-2 hover:text-slate-600 font-medium" to='addEmployee'>
                                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
                                                 <path d="M5.25 6.375a4.125 4.125 0 1 1 8.25 0 4.125 4.125 0 0 1-8.25 0ZM2.25 19.125a7.125 7.125 0 0 1 14.25 0v.003l-.001.119a.75.75 0 0 1-.363.63 13.067 13.067 0 0 1-6.761 1.873c-2.472 0-4.786-.684-6.76-1.873a.75.75 0 0 1-.364-.63l-.001-.122ZM18.75 7.5a.75.75 0 0 0-1.5 0v2.25H15a.75.75 0 0 0 0 1.5h2.25v2.25a.75.75 0 0 0 1.5 0v-2.25H21a.75.75 0 0 0 0-1.5h-2.25V7.5Z" />
                                             </svg>
@@ -66,7 +66,7 @@ export default function Layout() {
 
                                     {/* You */}
                                     <small className='font-semibold'>YOU</small>
-                                    <Link className="flex rounded gap-5 p-2 hover:bg-gray-600 font-medium">
+                                    <Link className="flex rounded gap-3 p-2 hover:text-slate-600 font-medium">
                                         <img src="https://cdn2.iconfinder.com/data/icons/ui-camera-set-from-iconspace-2/21/iconspace_Portrait_Mode_25px-512.png" alt="Employee Picture" className="rounded-full my-auto bg-white w-8 h-8" />
                                         <span className='my-auto text-sm'>{userProperties.displayName}</span>
                                     </Link>
@@ -75,7 +75,7 @@ export default function Layout() {
                                     <hr className='mt-2 mb-2' />
 
                                     <div>
-                                        <Link className="flex rounded gap-5 p-2 hover:bg-gray-600 font-medium">
+                                        <Link className="flex gap-3 p-2 hover:text-slate-600 font-medium">
                                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
                                                 <path fillRule="evenodd" d="M11.078 2.25c-.917 0-1.699.663-1.85 1.567L9.05 4.889c-.02.12-.115.26-.297.348a7.493 7.493 0 0 0-.986.57c-.166.115-.334.126-.45.083L6.3 5.508a1.875 1.875 0 0 0-2.282.819l-.922 1.597a1.875 1.875 0 0 0 .432 2.385l.84.692c.095.078.17.229.154.43a7.598 7.598 0 0 0 0 1.139c.015.2-.059.352-.153.43l-.841.692a1.875 1.875 0 0 0-.432 2.385l.922 1.597a1.875 1.875 0 0 0 2.282.818l1.019-.382c.115-.043.283-.031.45.082.312.214.641.405.985.57.182.088.277.228.297.35l.178 1.071c.151.904.933 1.567 1.85 1.567h1.844c.916 0 1.699-.663 1.85-1.567l.178-1.072c.02-.12.114-.26.297-.349.344-.165.673-.356.985-.57.167-.114.335-.125.45-.082l1.02.382a1.875 1.875 0 0 0 2.28-.819l.923-1.597a1.875 1.875 0 0 0-.432-2.385l-.84-.692c-.095-.078-.17-.229-.154-.43a7.614 7.614 0 0 0 0-1.139c-.016-.2.059-.352.153-.43l.84-.692c.708-.582.891-1.59.433-2.385l-.922-1.597a1.875 1.875 0 0 0-2.282-.818l-1.02.382c-.114.043-.282.031-.449-.083a7.49 7.49 0 0 0-.985-.57c-.183-.087-.277-.227-.297-.348l-.179-1.072a1.875 1.875 0 0 0-1.85-1.567h-1.843ZM12 15.75a3.75 3.75 0 1 0 0-7.5 3.75 3.75 0 0 0 0 7.5Z" clipRule="evenodd" />
                                             </svg>
@@ -88,7 +88,7 @@ export default function Layout() {
                                                 handleSignOut();
                                             }, 1000);
                                         }}
-                                            className="flex rounded gap-5 p-2 hover:bg-gray-600 font-medium"
+                                            className="flex rounded gap-3 p-2 hover:text-slate-600 font-medium"
                                         >
                                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
                                                 <path fillRule="evenodd" d="M9.53 2.47a.75.75 0 0 1 0 1.06L4.81 8.25H15a6.75 6.75 0 0 1 0 13.5h-3a.75.75 0 0 1 0-1.5h3a5.25 5.25 0 1 0 0-10.5H4.81l4.72 4.72a.75.75 0 1 1-1.06 1.06l-6-6a.75.75 0 0 1 0-1.06l6-6a.75.75 0 0 1 1.06 0Z" clipRule="evenodd" />
@@ -102,7 +102,7 @@ export default function Layout() {
                                 <div className="lg:hidden">
                                     <div className="mb-20">
                                         {/* Employee List */}
-                                        <Link title='Employee List' className="flex rounded gap-5 p-2 hover:bg-gray-600 font-medium" to='/'>
+                                        <Link title='Employees' className="flex rounded gap-3 p-2 hover:text-slate-600 font-medium" to='/'>
                                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
                                                 <path fillRule="evenodd" d="M8.25 6.75a3.75 3.75 0 1 1 7.5 0 3.75 3.75 0 0 1-7.5 0ZM15.75 9.75a3 3 0 1 1 6 0 3 3 0 0 1-6 0ZM2.25 9.75a3 3 0 1 1 6 0 3 3 0 0 1-6 0ZM6.31 15.117A6.745 6.745 0 0 1 12 12a6.745 6.745 0 0 1 6.709 7.498.75.75 0 0 1-.372.568A12.696 12.696 0 0 1 12 21.75c-2.305 0-4.47-.612-6.337-1.684a.75.75 0 0 1-.372-.568 6.787 6.787 0 0 1 1.019-4.38Z" clipRule="evenodd" />
                                                 <path d="M5.082 14.254a8.287 8.287 0 0 0-1.308 5.135 9.687 9.687 0 0 1-1.764-.44l-.115-.04a.563.563 0 0 1-.373-.487l-.01-.121a3.75 3.75 0 0 1 3.57-4.047ZM20.226 19.389a8.287 8.287 0 0 0-1.308-5.135 3.75 3.75 0 0 1 3.57 4.047l-.01.121a.563.563 0 0 1-.373.486l-.115.04c-.567.2-1.156.349-1.764.441Z" />
@@ -111,7 +111,7 @@ export default function Layout() {
                                         {/* End of Employee List */}
 
                                         {/* Employee List */}
-                                        <Link title='Add Employee' className="flex rounded gap-5 p-2 hover:bg-gray-600 font-medium" to='addEmployee'>
+                                        <Link title='Add Employee' className="flex rounded gap-3 p-2 hover:text-slate-600 font-medium" to='addEmployee'>
                                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
                                                 <path d="M5.25 6.375a4.125 4.125 0 1 1 8.25 0 4.125 4.125 0 0 1-8.25 0ZM2.25 19.125a7.125 7.125 0 0 1 14.25 0v.003l-.001.119a.75.75 0 0 1-.363.63 13.067 13.067 0 0 1-6.761 1.873c-2.472 0-4.786-.684-6.76-1.873a.75.75 0 0 1-.364-.63l-.001-.122ZM18.75 7.5a.75.75 0 0 0-1.5 0v2.25H15a.75.75 0 0 0 0 1.5h2.25v2.25a.75.75 0 0 0 1.5 0v-2.25H21a.75.75 0 0 0 0-1.5h-2.25V7.5Z" />
                                             </svg>
@@ -120,7 +120,7 @@ export default function Layout() {
                                     </div>
 
                                     {/* You */}
-                                    <Link title='You' className="flex rounded gap-5 p-2 hover:bg-gray-600 font-medium">
+                                    <Link title='You' className="flex rounded gap-3 p-2 font-medium">
                                         <img src="https://cdn2.iconfinder.com/data/icons/ui-camera-set-from-iconspace-2/21/iconspace_Portrait_Mode_25px-512.png" alt="Employee Picture" className="rounded-full bg-white w-6 h-6" />
                                     </Link>
                                     {/* End of You */}
@@ -129,7 +129,7 @@ export default function Layout() {
 
                                     <div>
                                         {/* Settings */}
-                                        <Link title='Settings' className="flex rounded gap-5 p-2 hover:bg-gray-600 font-medium">
+                                        <Link title='Settings' className="flex rounded gap-3 p-2 hover:text-slate-600 font-medium">
                                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
                                                 <path fillRule="evenodd" d="M11.078 2.25c-.917 0-1.699.663-1.85 1.567L9.05 4.889c-.02.12-.115.26-.297.348a7.493 7.493 0 0 0-.986.57c-.166.115-.334.126-.45.083L6.3 5.508a1.875 1.875 0 0 0-2.282.819l-.922 1.597a1.875 1.875 0 0 0 .432 2.385l.84.692c.095.078.17.229.154.43a7.598 7.598 0 0 0 0 1.139c.015.2-.059.352-.153.43l-.841.692a1.875 1.875 0 0 0-.432 2.385l.922 1.597a1.875 1.875 0 0 0 2.282.818l1.019-.382c.115-.043.283-.031.45.082.312.214.641.405.985.57.182.088.277.228.297.35l.178 1.071c.151.904.933 1.567 1.85 1.567h1.844c.916 0 1.699-.663 1.85-1.567l.178-1.072c.02-.12.114-.26.297-.349.344-.165.673-.356.985-.57.167-.114.335-.125.45-.082l1.02.382a1.875 1.875 0 0 0 2.28-.819l.923-1.597a1.875 1.875 0 0 0-.432-2.385l-.84-.692c-.095-.078-.17-.229-.154-.43a7.614 7.614 0 0 0 0-1.139c-.016-.2.059-.352.153-.43l.84-.692c.708-.582.891-1.59.433-2.385l-.922-1.597a1.875 1.875 0 0 0-2.282-.818l-1.02.382c-.114.043-.282.031-.449-.083a7.49 7.49 0 0 0-.985-.57c-.183-.087-.277-.227-.297-.348l-.179-1.072a1.875 1.875 0 0 0-1.85-1.567h-1.843ZM12 15.75a3.75 3.75 0 1 0 0-7.5 3.75 3.75 0 0 0 0 7.5Z" clipRule="evenodd" />
                                             </svg>
@@ -143,7 +143,7 @@ export default function Layout() {
                                             }, 1000);
                                         }}
                                             title='Sign Out'
-                                            className="flex rounded gap-5 p-2 hover:bg-gray-600 font-medium"
+                                            className="flex rounded gap-3 p-2 hover:text-slate-600 font-medium"
                                         >
                                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
                                                 <path fillRule="evenodd" d="M9.53 2.47a.75.75 0 0 1 0 1.06L4.81 8.25H15a6.75 6.75 0 0 1 0 13.5h-3a.75.75 0 0 1 0-1.5h3a5.25 5.25 0 1 0 0-10.5H4.81l4.72 4.72a.75.75 0 1 1-1.06 1.06l-6-6a.75.75 0 0 1 0-1.06l6-6a.75.75 0 0 1 1.06 0Z" clipRule="evenodd" />
@@ -159,10 +159,9 @@ export default function Layout() {
 
                             <>
                                 <div className="hidden lg:block">
-
-                                    <span className='font-medium'>Menu</span>
+                                    <span className='font-light'>Menu</span>
                                     {/* Sign In */}
-                                    <Link className="flex rounded gap-5 p-2 hover:bg-gray-600 font-medium" to='signIn'>
+                                    <Link className="flex gap-3 p-2 hover:text-slate-600 font-medium" to='signIn'>
                                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
                                             <path fillRule="evenodd" d="M16.72 7.72a.75.75 0 0 1 1.06 0l3.75 3.75a.75.75 0 0 1 0 1.06l-3.75 3.75a.75.75 0 1 1-1.06-1.06l2.47-2.47H3a.75.75 0 0 1 0-1.5h16.19l-2.47-2.47a.75.75 0 0 1 0-1.06Z" clipRule="evenodd" />
                                         </svg>
@@ -171,7 +170,7 @@ export default function Layout() {
                                     {/* End of Sign In */}
 
                                     {/* Sign Up */}
-                                    <Link className="flex rounded gap-5 p-2 hover:bg-gray-600 font-medium" to='signUp'>
+                                    <Link className="flex gap-3 p-2 hover:text-slate-600 font-medium" to='signUp'>
                                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
                                             <path d="M21.731 2.269a2.625 2.625 0 0 0-3.712 0l-1.157 1.157 3.712 3.712 1.157-1.157a2.625 2.625 0 0 0 0-3.712ZM19.513 8.199l-3.712-3.712-8.4 8.4a5.25 5.25 0 0 0-1.32 2.214l-.8 2.685a.75.75 0 0 0 .933.933l2.685-.8a5.25 5.25 0 0 0 2.214-1.32l8.4-8.4Z" />
                                             <path d="M5.25 5.25a3 3 0 0 0-3 3v10.5a3 3 0 0 0 3 3h10.5a3 3 0 0 0 3-3V13.5a.75.75 0 0 0-1.5 0v5.25a1.5 1.5 0 0 1-1.5 1.5H5.25a1.5 1.5 0 0 1-1.5-1.5V8.25a1.5 1.5 0 0 1 1.5-1.5h5.25a.75.75 0 0 0 0-1.5H5.25Z" />
@@ -183,7 +182,7 @@ export default function Layout() {
 
                                 <div className="lg:hidden">
                                     {/* Sign In */}
-                                    <Link title='Sign In' className="flex rounded gap-5 p-2 hover:bg-gray-600 font-medium" to='SignIn'>
+                                    <Link title='Sign In' className="flex rounded gap-3 p-2 hover:text-slate-600 font-medium" to='SignIn'>
                                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
                                             <path fillRule="evenodd" d="M16.72 7.72a.75.75 0 0 1 1.06 0l3.75 3.75a.75.75 0 0 1 0 1.06l-3.75 3.75a.75.75 0 1 1-1.06-1.06l2.47-2.47H3a.75.75 0 0 1 0-1.5h16.19l-2.47-2.47a.75.75 0 0 1 0-1.06Z" clipRule="evenodd" />
                                         </svg>
@@ -191,7 +190,7 @@ export default function Layout() {
                                     {/* End of Sign In */}
 
                                     {/* Sign Up */}
-                                    <Link title='Sign Up' className="flex rounded gap-5 p-2 hover:bg-gray-600 font-medium" to='signUp'>
+                                    <Link title='Sign Up' className="flex rounded gap-3 p-2 hover:text-slate-600 font-medium" to='signUp'>
                                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
                                             <path d="M21.731 2.269a2.625 2.625 0 0 0-3.712 0l-1.157 1.157 3.712 3.712 1.157-1.157a2.625 2.625 0 0 0 0-3.712ZM19.513 8.199l-3.712-3.712-8.4 8.4a5.25 5.25 0 0 0-1.32 2.214l-.8 2.685a.75.75 0 0 0 .933.933l2.685-.8a5.25 5.25 0 0 0 2.214-1.32l8.4-8.4Z" />
                                             <path d="M5.25 5.25a3 3 0 0 0-3 3v10.5a3 3 0 0 0 3 3h10.5a3 3 0 0 0 3-3V13.5a.75.75 0 0 0-1.5 0v5.25a1.5 1.5 0 0 1-1.5 1.5H5.25a1.5 1.5 0 0 1-1.5-1.5V8.25a1.5 1.5 0 0 1 1.5-1.5h5.25a.75.75 0 0 0 0-1.5H5.25Z" />
@@ -213,20 +212,12 @@ export default function Layout() {
                 <small>Developed by <strong>Ferangelo Tuason</strong></small>
 
                 <div className="flex gap-3 mt-1">
-                    <a className='hover:text-gray-300' href="https://www.facebook.com/gelo.tuason26/" target="_blank">
-                        <FontAwesomeIcon icon={faFacebookF} />
-                    </a>
-
                     <a className='hover:text-gray-300' href="https://www.linkedin.com/in/ferangelo-tuason/" target="_blank">
                         <FontAwesomeIcon icon={faLinkedinIn} />
                     </a>
-
-                    <a className='hover:text-gray-300' href="https://www.instagram.com/gelotuason/?hl=en" target="_blank">
-                        <FontAwesomeIcon icon={faInstagram} />
-                    </a>
-
-                    <a className='hover:text-gray-300' href="https://www.tiktok.com/@gelotuason" target="_blank">
-                        <FontAwesomeIcon icon={faTiktok} />
+                    
+                    <a className='hover:text-gray-300' href="https://github.com/gelotuason" target="_blank">
+                        <FontAwesomeIcon icon={faGithub} />
                     </a>
 
                     <a className='hover:text-gray-300' href="mailto:gelotuason@gmail.com" target="_blank">
